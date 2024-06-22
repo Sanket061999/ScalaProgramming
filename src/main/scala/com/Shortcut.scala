3005bb873ee0fdcebec90e7632ab973c71cf0647
“Sanket Deshmukh” <Sanket.Deshmukh@landisgyr.com>
2024-06-23 01:39:30 +0530

    short cut

---
+++ src/main/scala/com/Shortcut.scala
@@ -0,0 +1,10 @@
+package com
+
+object Shortcut {
+  def main(args: Array[String]): Unit = {
+    //ctrl + alt +L for format the code
+    //ctrl + d == ctrl + c and ctrl +v
+    //ctrl + y // for delete the line
+
+  }
+}
